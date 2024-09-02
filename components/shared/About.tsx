e3a824cfd41ba08a711f794a6567fe2fba5fbe52
Muhammad Sharyar <msharyar45@gmail.com>
2024-09-02 12:46:41 +0400

    update about section

--- components/shared/About.tsx
+++ components/shared/About.tsx
@@ -12,14 +12,14 @@ const About = () => {
       <div className="w-full md:w-1/2">
         <Heading title="About Me" className="text-5xl font-bold mb-7" />
         <p className="text-sm mb-4">
-          Innovative and driven Web developer and UI/UX Designer with over 5
-          years of experience with a passion for crafting robust and scalable
-          solutions. Equipped with a strong foundation in full-stack
-          development, I am adept at designing and implementing high-quality
-          code across multiple platforms. With a track record of successfully
-          delivering projects on time and exceeding client expectations, I am
-          now seeking new opportunities to leverage my skills and contribute to
-          cutting-edge technological advancements.
+          Innovative and result-driven full-stack developer with over 5 years of
+          experience with a passion for crafting robust and scalable solutions.
+          Equipped with a strong foundation in full-stack development, I am
+          adept at designing and implementing high-quality code across multiple
+          platforms. With a track record of successfully delivering projects on
+          time and exceeding client expectations, I am now seeking new
+          opportunities to leverage my skills and contribute to cutting-edge
+          technological advancements.
         </p>
         <Link href={process.env.NEXT_PUBLIC_CV_LINK!} download>
           <Button>Download CV</Button>
